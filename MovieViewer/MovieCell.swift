10153611bbc2c1876a572d6f34ada3b5a8820148
seminole6 <Devon@Devons-MacBook-Pro.local>
2016-01-30 19:55:55 -0500

    Began the application by adding a tableview that loads the titles and overviews of all the new releases.

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Devon Maguire on 1/30/16.
+//  Copyright © 2016 Devon Maguire. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
